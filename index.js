7bb288b48c6c84882ce203f61bc8a58741503cb0
pedooor013 <pedrolopes.programador013@gmail.com>
2025-03-12 12:59:18 -0300

    funcionalidade do carrossel (nao terminada)

---
+++ index.js
@@ -0,0 +1,9 @@
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+var setaDireita = window.document.getElementById("seta-direita")
+var imgScooter = window.document.getElementById("img-scooter")
+var img
+var imgCb500 = window.document.getElementById("img-cb500")
+
+function rolarParaEsquerda(){
+
+}
