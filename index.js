c776724339232b00d553dc587f9119079d5d9aaf
pedooor013 <pedrolopes.programador013@gmail.com>
2025-03-13 13:02:22 -0300

    Criação do sistema de overlay e do menu saiba mais sobre as informações da moto CB 500

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var setaEsquerda = window.document.getElementById("seta-esquerda")
 var setaDireita = window.document.getElementById("seta-direita")
 var imgScooter = window.document.getElementById("img-scooter")
-var img
 var imgCb500 = window.document.getElementById("img-cb500")
 
-function rolarParaEsquerda(){
+function rolarParaDireita(){
+    imgScooter.style = "display:none"
+    imgCb500.style = "display:flex"
+}
 
+function rolarParaEsquerda(){
+    imgScooter.style = "display:flex"
+    imgCb500.style = "display:none"
 }
