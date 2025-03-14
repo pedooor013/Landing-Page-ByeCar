2e196d0dc2591cf72ad62d95f6a709bd8ed69256
pedooor013 <pedrolopes.programador013@gmail.com>
2025-03-14 13:05:28 -0300

    finalização do projeto ByeCar - Funcionalidade carrossel e overlay implementadas

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ var setaEsquerda = window.document.getElementById("seta-esquerda")
 var setaDireita = window.document.getElementById("seta-direita")
 var imgScooter = window.document.getElementById("img-scooter")
 var imgCb500 = window.document.getElementById("img-cb500")
+var overlay = window.document.getElementById("overlay")
+
 
 function rolarParaDireita(){
     imgScooter.style = "display:none"
     imgCb500.style = "display:flex"
-}
+}   
 
 function rolarParaEsquerda(){
     imgScooter.style = "display:flex"
     imgCb500.style = "display:none"
+}
+
+function abrirOverlay(){
+    overlay.style = "display:flex"      
+}
+function fecharOverlay(){
+    overlay.style = "display:none"
 }
